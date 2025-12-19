0cac21e304290115ff414c0e79f9b8a49e6016ac
Sebastiaan Balfoort <balfoortsebastiaan@gmail.com>
2025-12-19 10:29:30 +0100

    changed caching so you cant scroll under shop

--- src/create-invite-url.mjs
+++ src/create-invite-url.mjs
@@ -7,7 +7,7 @@ if (!clientId) {
   process.exit(1);
 }
 
-// Rechten: bot heeft nodig: ManageRoles om role te kunnen toekennen
+
 const permissions = BigInt(PermissionFlagsBits.ManageRoles | PermissionFlagsBits.SendMessages | PermissionFlagsBits.ViewChannel);
 const scope = 'bot%20applications.commands';
 const url = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&permissions=${permissions}&scope=${scope}`;
